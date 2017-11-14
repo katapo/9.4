9ded5325ac812e7a7608a2a4688d058942d23bc1
katapo <kata.pochec@gmail.com>
2017-11-14 11:59:44 +0100

    first commit

---
+++ 9.4.js
@@ -0,0 +1,13 @@
+function drawTree(x) {
+  for (var i = 1 ; i <= x ; i++) {
+    
+    var star = '';
+    
+    for (var j = 0 ; j < i ; j++ ) {
+      star += '*'
+    }
+    console.log(star);
+  }
+}
+
+drawTree(5);
