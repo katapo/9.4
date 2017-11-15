e38db4924cd2b07d5a401600c96e6ac1a376fe4b
katapo <kata.pochec@gmail.com>
2017-11-16 00:53:25 +0100

    third commit

--- 9.4.js
+++ 9.4.js
@@ -1,10 +1,10 @@
 function drawTree(treeHeight) {
-  for (var i = 1; i <= x; i++) {
+  for (var i = 1; i <= treeHeight; i++) {
     
     var star = '';
     
-    for (var j = 0; j < i; j++ ) {
-      star += '*'
+    for (var j = 0; j < i; j++) {
+      star += '*';
     }
     console.log(star);
   }
